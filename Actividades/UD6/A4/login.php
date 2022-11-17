93f7a1bd00c61c7d0de8942fd3c8e09b893a6095
Xamargona <Jamargon14@gmail.com>
2022-11-17 19:41:25 +0100

    UPDATE A4

---
+++ Actividades/UD6/A4/login.php
@@ -0,0 +1,17 @@
+<?php
+    // Creamos la sesión
+    ini_set('session.cache_expire', 10);
+    session_start();
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login - Javier Martínez González</title>
+</head>
+<body>
+    
+</body>
+</html>
